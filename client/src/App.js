715b3c757048ffe4050c1e9823cf18b262e68570
fvizpal <vishalp9966@gmail.com>
2023-12-26 13:39:50 +0530

    adding scenes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "scenes/layout";
+import AuthPage from "scenes/authPage";
+import Dashboard from "scenes/dashboard";
+
 function App() {
-    return <div className="app"></div>;
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<AuthPage />} />
+                    <Route element={<Layout />}>
+                        <Route path="/dashboard" element={<Dashboard />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
